d236888f2693f219794388bb4e31f5bd0958c768
Christopher Kites <chriskites@gmail.com>
2019-01-09 18:39:13 -0800

    Finish code for sec_02.09

---
+++ sec_02.09/main.c
@@ -0,0 +1,16 @@
+/*
+ * File: main.c
+ * Author: Christopher Kites (chriskites@gmail.com)
+ *
+ * Created: 2019/01/09 (PST)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ * */
+int main(){
+        printf("Hello World\n");
+        return (EXIT_SUCCESS);
+}
