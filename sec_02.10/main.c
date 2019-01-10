eb2e0114e2127f1371f868a34729528e20e7458d
Christopher Kites <chriskites@gmail.com>
2019-01-09 19:07:31 -0800

    Finish code for sec_02.10

---
+++ sec_02.10/main.c
@@ -0,0 +1,18 @@
+/*
+ * File: main.c
+ * Author: Christopher Kites (chriskites@gmail.com)
+ * */
+
+#include <stdio.h>
+
+int main() {
+        printf("Please provide a number:");
+        int aVar = 0;
+        scanf("%d", &aVar);
+        printf("\nYour number was: %d\n", aVar);
+        float x;
+        x = 3.56;
+        double d;
+
+        return 0;
+}
