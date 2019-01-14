80d1b7ea2954735244d7a66cb3fa3baa9f738ba6
Christopher Kites <chriskites@gmail.com>
2019-01-13 18:12:47 -0800

    Finish code for sec_02.15

---
+++ sec_02.15/average.c
@@ -0,0 +1,23 @@
+/**
+Instructions: 
+Write a program that will input 3 integer values from user and then will 
+display the average of them. You would require to declare 3 integer variables 
+for holding the integers given by the users from keyboard and then you need 
+another variable for holding the average. Decide by yourself about the datatype 
+of the average variable.
+
+Finally print the average into console using printf.
+*//*
+
+#include <stdio.h>
+
+int main()
+{
+	
+	
+
+	// complete the program as per instructions.
+
+
+	return 0;
+}*/
