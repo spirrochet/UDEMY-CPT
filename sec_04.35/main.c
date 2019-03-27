5d4f7a9b08e1c43557bda913cc2af0e8e431ce84
Christopher Kites <chriskites@gmail.com>
2019-03-27 11:22:11 -0700

    -S
    Finish sec 4.34

---
+++ sec_04.35/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+        printf("Hello, World!\n");
+        int n, sum, i;
+        printf("Enter n: ");
+        scanf("%d", &n);
+
+        sum = 0;
+        return 0;
+}
